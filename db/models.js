1449a701bf9f37afa097296512b7bdd62d1015d9
ArthurBoyle <75610914+ArthurBoyle@users.noreply.github.com>
2021-02-16 22:15:09 +0800

    feat：创建后台注册与登录接口

---
+++ db/models.js
@@ -0,0 +1,36 @@
+/*
+测试使用mongoose操作mongodb数据库
+1. 连接数据库
+  1.1. 引入mongoose
+  1.2. 连接指定数据库(URL只有数据库是变化的)
+  1.3. 获取连接对象
+  1.4. 绑定连接完成的监听(用来提示连接成功)
+2. 得到对应特定集合的Model
+  2.1. 字义Schema(描述文档结构)
+  2.2. 定义Model(与集合对应, 可以操作集合)
+3. 通过Model或其实例对集合数据进行CRUD操作
+  3.1. 通过Model实例的save()添加数据
+  3.2. 通过Model的find()/findOne()查询多个或一个数据
+  3.3. 通过Model的findByIdAndUpdate()更新某个数据
+  3.4. 通过Model的remove()删除匹配的数据
+ */
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/recruit", {useNewUrlParser: true, useUnifiedTopology: true});
+const conn = mongoose.connection;
+conn.on("connected", () => {
+    console.log("MongoDB connect success !");
+});
+
+const userSchema = mongoose.Schema({
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    type: {type: String, required: true},
+    header: {type: String}, //头像名称
+    post: {type: String},   //职位
+    info: {type: String},   //个人或职位简介
+    company: {type: String},
+    salary: {type: String}
+});
+const UserModel = mongoose.model("user", userSchema);
+exports.UserModel = UserModel;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
+const {UserModel} = require("../db/models");
+const md5 = require("blueimp-md5");
+const filter = {password: 0, __v: 0}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+//test
+// router.post("/register", function (req, res) {
+//   const {username, password} = req.body;
+//   if(username === "admin") {
+//     res.send({code: 1, msg: "该用户已存在"});
+//   } else {
+//     res.send({code: 0, data: {id: "123", username, password}});
+//   }
+// })
+
+//注册的路由
+router.post("/register", function (req, res) {
+  const {username, password, type} = req.body;
+  UserModel.findOne({username}, function (error, user) {
+    if(user) {
+      res.send({code: 1, msg: "此用户已存在"});
+    } else {
+      new UserModel({username, password: md5(password), type}).save(function (error, user) {
+        res.cookie("userid", user._id, {maxAge: 1000*60*60});
+        const data = {username, type, _id: user._id};
+        res.send({code: 0, data});
+      })
+    }
+  })
+})
+
+//登录的路由
+router.post("/login", function (req, res) {
+  const {username, password} = req.body;
+  UserModel.findOne({username, password: md5(password)}, filter, function (error, user) {
+    if(user) {
+      res.cookie("userid", user._id, {maxAge: 1000*60*60});
+      res.send({code: 0, data: user})
+    } else {
+      res.send({code: 1, msg: "用户名或密码不正确！"})
+    }
+  })
+})
 
 module.exports = router;
