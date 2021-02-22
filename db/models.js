1cc852d7d53d187d0d39ca93167a8d0e77452643
ArthurBoyle <75610914+ArthurBoyle@users.noreply.github.com>
2021-02-22 17:52:32 +0800

    feat：实现消息互动

--- db/models.js
+++ db/models.js
@@ -15,6 +15,7 @@
   3.4. 通过Model的remove()删除匹配的数据
  */
 
+//连接数据库
 const mongoose = require("mongoose");
 mongoose.connect(
     "mongodb://localhost:27017/recruit",
@@ -28,6 +29,7 @@ conn.on("connected", () => {
     console.log("MongoDB connect success !");
 });
 
+//定义users集合的文档结构
 const userSchema = mongoose.Schema({
     username: {type: String, required: true},
     password: {type: String, required: true},
@@ -40,3 +42,15 @@ const userSchema = mongoose.Schema({
 });
 const UserModel = mongoose.model("user", userSchema);
 exports.UserModel = UserModel;
+
+//定义chats集合的文档结构
+const chatSchema = mongoose.Schema({
+    from: {type: String, required: true},
+    to: {type: String, required: true},
+    chat_id: {type: String, required: true},
+    content: {type: String, required: true},
+    read: {type: Boolean, default: false},
+    create_time: {type: Number}
+});
+const ChatModel = mongoose.model("chat", chatSchema);
+exports.ChatModel = ChatModel;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const {UserModel} = require("../db/models");
+const {UserModel, ChatModel} = require("../db/models");
 const md5 = require("blueimp-md5");
 const filter = {password: 0, __v: 0}
 
@@ -42,9 +42,9 @@ router.post("/login", function (req, res) {
   UserModel.findOne({username, password: md5(password)}, filter, function (error, user) {
     if(user) {
       res.cookie("userid", user._id, {maxAge: 1000*60*60});
-      res.send({code: 0, data: user})
+      res.send({code: 0, data: user});
     } else {
-      res.send({code: 1, msg: "用户名或密码不正确！"})
+      res.send({code: 1, msg: "用户名或密码不正确！"});
     }
   })
 })
@@ -62,8 +62,8 @@ router.post("/update", function (req, res) {
       res.send({code: 1, msg: "请先登录"});
     } else {
       const {username, type, _id} = oldUser;
-      const data = Object.assign({username, type, _id}, user)
-      res.send({code: 0, data})
+      const data = Object.assign({username, type, _id}, user);
+      res.send({code: 0, data});
     }
   })
 })
@@ -72,11 +72,11 @@ router.post("/update", function (req, res) {
 router.get("/user", function (req, res) {
   const userid = req.cookies.userid;
   if (!userid) {
-    return res.send({code: 1, msg: "请先登录"})
+    return res.send({code: 1, msg: "请先登录"});
   }
   UserModel.findOne({_id: userid}, filter, function (error, user) {
     if (user) {
-      res.send({code: 0, data: user})
+      res.send({code: 0, data: user});
     }
   });
 })
@@ -86,10 +86,40 @@ router.get("/userlist", function (req, res) {
   const {type} = req.query;
   UserModel.find({type}, filter, function (error, users) {
     if (type !== "boss" && type !== "expert") {
-      res.send({code: 1, msg: "用户类型错误"})
+      res.send({code: 1, msg: "用户类型错误"});
     } else {
-      res.send({code: 0, data: users})
+      res.send({code: 0, data: users});
     }
+  })
+})
+
+//获取当前用户的聊天消息列表
+router.get("/msglist", function (req, res) {
+  const userid = req.cookies.userid;
+  UserModel.find(function (error, userDocs) {
+
+    // const users = {};
+    // userDocs.map(doc => {
+    //   users[doc._id] = {username: doc.usernama, header: doc.header}
+    // });
+    const users = userDocs.reduce((users, user) => {
+      users[user._id] = {username: user.username, header: user.header};
+      return users;
+    }, {})
+
+    ChatModel.find({"$or": [{from: userid}, {to: userid}]}, filter, function (error, chatMsgs) {
+      res.send({code: 0, data: {users, chatMsgs}});
+    })
+  })
+})
+
+//修改指定消息为已读
+router.post("/readmsg", function (req, res) {
+  const from = req.body.from;
+  const to = req.cookies.userid;
+  ChatModel.update({from, to, read: false}, {read: true}, {multi: true}, function (error, doc) {
+    console.log("/readmsg", doc);
+    res.send({code: 0, data: doc.nModified});
   })
 })
 
